4a8547f8ebbd126b1715505ba8190f65f0898f1d
Supreet <supreetkumar32@gmail.com>
2025-12-25 21:48:24 +0530

    first commit

---
+++ src/main/java/com/hotelbooking/projects/HotelHub/HotelHubApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbooking.projects.HotelHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelbooking/projects/HotelHub/HotelHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelbooking.projects.HotelHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
